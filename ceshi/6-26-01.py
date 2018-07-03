e9e248e30f49fa1482c6ebbac4fe1e6e211b712f
赵洪达 <18519122475@163.com>
2018-07-03 17:05:39 +0800

    7-02

---
+++ ceshi/6-26-01.py
@@ -0,0 +1,21 @@
+import logging
+
+def foo(s):
+	return 10 / int(s)
+
+def bar(s):
+	return foo(s) * 2
+
+def main():
+	try:
+		bar(0)
+	
+	except Exception as e:
+		logging.exception(e)
+		
+		print('Error:', e)
+	finally:
+		print('finally')
+		
+
+main()
