0ac73b656674c59f0f21b446bec66d1f972a67a8
赵洪达 <18519122475@163.com>
2018-12-18 14:37:02 +0800

    12-17

---
+++ ceshi/12-17-01.py
@@ -0,0 +1,16 @@
+class Animal(object):
+	def run(self):
+		print('Animal is running')
+		
+		
+class Dog(Animal):
+	def run(self):
+		print('Dog is running')
+
+class Cat(Animal):
+	pass
+
+dog = Dog()
+dog.run()
+
+print(type(dog))
